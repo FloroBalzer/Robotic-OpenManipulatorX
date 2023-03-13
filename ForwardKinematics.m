65ea14ed6a351d536e66ca7019af769cd194a2bb
FloroBalzer <94688174+FloroBalzer@users.noreply.github.com>
2023-03-13 21:12:17 +0000

    Updates

--- ForwardKinematics.m
+++ ForwardKinematics.m
@@ -3,11 +3,11 @@ function [T01, T12, T23, T34, T45, T02, T03, T04, T05] = ForwardKinematics(t1, t
 % DH parameters
 t0 = atand(0.024/0.128);
 
-a0 = 0;       alpha0 = 0;     d1 = 0.077;     theta1 = t1;              % Link 1
-a1 = 0;       alpha1 = 90;    d2 = 0;         theta2 = t2-t0-79;        % Link 2
-a2 = 0.128;   alpha2 = 0;     d3 = 0;         theta3 = t3+t0-101;       % Link 3
-a3 = 0.148;   alpha3 = 0;     d4 = 0;         theta4 = t4-180;          % Link 4
-a4 = 0.126;   alpha4 = 0;     d5 = 0;         theta5 = 0;               % Gripper
+a0 = 0;      alpha0 = 0;     d1 = 7.7;     theta1 = t1;              % Link 1
+a1 = 0;      alpha1 = 90;    d2 = 0;       theta2 = t2-(90-t0);      %Link 2
+a2 = 13.0;   alpha2 = 0;     d3 = 0;       theta3 = t3-(90+t0);     %Link 3
+a3 = 12.4;   alpha3 = 0;     d4 = 0;       theta4 = t4-180;          % Link 4
+a4 = 12.6;   alpha4 = 0;     d5 = 0;       theta5 = 0;               % Gripper
 
 
 
@@ -22,9 +22,9 @@ T02 = T01*T12;
 T03 = T01*(T12*T23);
 T04 = T01*(T12*(T23*T34));
 T05 = T01*(T12*(T23*(T34*T45)));
-% fprintf("F_Theta1: %4.2f ", theta1);
-% fprintf("F_Theta2: %4.2f ", theta2);
-% fprintf("F_Theta3: %4.2f ", theta3);
-% fprintf("F_Theta4: %4.2f\n ", theta4);
+fprintf("F_Theta1: %4.2f ", theta1);
+fprintf("F_Theta2: %4.2f ", theta2);
+fprintf("F_Theta3: %4.2f ", theta3);
+fprintf("F_Theta4: %4.2f\n ", theta4);
 
 end
--- Simulation.m
+++ Simulation.m
@@ -3,9 +3,9 @@ clf
 %% Coordinate Frame Nested Function
     function CoordinateFrame(T, n_origin, n_origin_old)
         
-        X_pos = n_origin + transpose([T(1,1:end-1); T(2,1:end-1); T(3,1:end-1)]*[0.05; 0; 0]);
-        Y_pos = n_origin + transpose([T(1,1:end-1); T(2,1:end-1); T(3,1:end-1)]*[0; 0.05; 0]);
-        Z_pos = n_origin + transpose([T(1,1:end-1); T(2,1:end-1); T(3,1:end-1)]*[0; 0; 0.05]);
+        X_pos = n_origin + transpose([T(1,1:end-1); T(2,1:end-1); T(3,1:end-1)]*[5; 0; 0]);
+        Y_pos = n_origin + transpose([T(1,1:end-1); T(2,1:end-1); T(3,1:end-1)]*[0; 5; 0]);
+        Z_pos = n_origin + transpose([T(1,1:end-1); T(2,1:end-1); T(3,1:end-1)]*[0; 0; 5]);
         
         %plot Coordinate Frame
         X_val = [X_pos(1,1), n_origin(1,1)]; Y_val = [X_pos(1,2), n_origin(1,2)]; Z_val = [X_pos(1,3), n_origin(1,3)];
@@ -21,10 +21,10 @@ clf
     end
 %% Plot Set up
 grid on
-xlim([-0.40, 0.40]);
-ylim([-0.40, 0.40]); 
-zlim([-0.4, 0.80]);
-view(135, 45);
+xlim([-40, 40]);
+ylim([-40, 40]); 
+zlim([0, 80]);
+view(0, 0);
 
 %calculate Forward Kinematics
 [T01, T12, T23, T34,T45, T02, T03, T04, T05] = ForwardKinematics(t1, t2, t3, t4);%(t1, t2, t3, t4);
@@ -35,9 +35,9 @@ hold on
 %% Base
 base = [0; 0; 0; 1];
 origin = [0, 0, 0];
-X_pos = [0.05, 0, 0];
-Y_pos = [0, 0.05, 0];
-Z_pos = [0, 0, 0.05];
+X_pos = [5, 0, 0];
+Y_pos = [0, 5, 0];
+Z_pos = [0, 0, 5];
 
 X_val = [X_pos(1,1), origin(1,1)]; Y_val = [X_pos(1,2), origin(1,2)]; Z_val = [X_pos(1,3), origin(1,3)];
 plot3(X_val, Y_val, Z_val,'LineWidth', 1, 'Color', [0.4660 0.6740 0.1880]);
@@ -92,8 +92,8 @@ CoordinateFrame(T05, n_origin, n_origin_old);
 % disp(T34);
 % disp('T45');
 % disp(T45);
-disp('T04');
-disp(T04);
+% disp('T04');
+% disp(T04);
 disp('T05');
 disp(T05);
 hold off
