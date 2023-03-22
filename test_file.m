5e9df927c3b706965824a58df8bfd36bbc9c7fb1
FloroBalzer <94688174+FloroBalzer@users.noreply.github.com>
2023-03-22 15:38:58 +0000

    Final Changes for submission

--- test_file.m
+++ test_file.m
@@ -5,57 +5,57 @@ point_matrix = [];
 
 %start here
 start_pos = [10, 0, 5, 1/sqrt(2)];% format [x, y, z, sin(angle)]
-end_pos = [10, -15, 5, 1/sqrt(2)]; % format [x, y, z, sin(angle)]
+end_pos = [10, -10, 5, 1/sqrt(2)]; % format [x, y, z, sin(angle)]
 point_matrix = [point_matrix; start_pos, end_pos, 10, 1, 0, 0, 0];
 
-start_pos = [10, -15, 5, 1/sqrt(2)];% format [x, y, z, sin(angle)]
-end_pos = [25, -15, 5, 1/sqrt(2)]; % format [x, y, z, sin(angle)]
+start_pos = [10, -10, 5, 1/sqrt(2)];% format [x, y, z, sin(angle)]
+end_pos = [20, -10, 5, 1/sqrt(2)]; % format [x, y, z, sin(angle)]
 point_matrix = [point_matrix; start_pos, end_pos, 10, 1, 0, 0, 0];
 
-start_pos = [25, -15, 5, 1/sqrt(2)];% format [x, y, z, sin(angle)]
-end_pos = [25, 0, 5, 1/sqrt(2)]; % format [x, y, z, sin(angle)]
+start_pos = [20, -10, 5, 1/sqrt(2)];% format [x, y, z, sin(angle)]
+end_pos = [20, 0, 5, 1/sqrt(2)]; % format [x, y, z, sin(angle)]
 point_matrix = [point_matrix; start_pos, end_pos, 10, 1, 0, 0, 0];
 
 
-start_pos = [25, 0, 5, 1/sqrt(2)];% format [x, y, z, sin(angle)]
+start_pos = [20, 0, 5, 1/sqrt(2)];% format [x, y, z, sin(angle)]
 end_pos = [10, 0, 5, 1/sqrt(2)]; % format [x, y, z, sin(angle)]
 point_matrix = [point_matrix; start_pos, end_pos, 10, 1, 0, 0, 0];
 
 
 start_pos = [10, 0, 5, 1/sqrt(2)];% format [x, y, z, sin(angle)]
-end_pos = [10, 0, 20, 1/sqrt(2)]; % format [x, y, z, sin(angle)]
+end_pos = [10, 0, 15, 1/sqrt(2)]; % format [x, y, z, sin(angle)]
 point_matrix = [point_matrix; start_pos, end_pos, 10, 1, 0, 0, 0];
 
 
-start_pos = [10, 0, 20, 1/sqrt(2)];% format [x, y, z, sin(angle)]
-end_pos = [25, 0, 20, 0]; % format [x, y, z, sin(angle)]
+start_pos = [10, 0, 15, 1/sqrt(2)];% format [x, y, z, sin(angle)]
+end_pos = [20, 0, 15, 0]; % format [x, y, z, sin(angle)]
 point_matrix = [point_matrix; start_pos, end_pos, 10, 1, 0, 0, 0];
 
 
-start_pos = [25, 0, 20, 0];% format [x, y, z, sin(angle)]
-end_pos = [25, 0, 5, 1/sqrt(2)]; % format [x, y, z, sin(angle)]
+start_pos = [20, 0, 15, 0];% format [x, y, z, sin(angle)]
+end_pos = [20, 0, 5, 1/sqrt(2)]; % format [x, y, z, sin(angle)]
 point_matrix = [point_matrix; start_pos, end_pos, 10, 1, 0, 0, 0];
 
 
-start_pos = [25, 0, 5, 1/sqrt(2)];% format [x, y, z, sin(angle)]
+start_pos = [20, 0, 5, 1/sqrt(2)];% format [x, y, z, sin(angle)]
 end_pos = [10, 0, 5, 1/sqrt(2)]; % format [x, y, z, sin(angle)]
 point_matrix = [point_matrix; start_pos, end_pos, 10, 1, 0, 0, 0];
 
 
 start_pos = [10, 0, 5, 1/sqrt(2)];% format [x, y, z, sin(angle)]
-end_pos = [10, 0, 20, 1/sqrt(2)]; % format [x, y, z, sin(angle)]
+end_pos = [10, 0, 15, 1/sqrt(2)]; % format [x, y, z, sin(angle)]
 point_matrix = [point_matrix; start_pos, end_pos, 10, 1, 0, 0, 0];
 
-start_pos = [10, 0, 20, 1/sqrt(2)];% format [x, y, z, sin(angle)]
-end_pos = [10, -15, 20, 1/sqrt(2)]; % format [x, y, z, sin(angle)]
+start_pos = [10, 0, 15, 1/sqrt(2)];% format [x, y, z, sin(angle)]
+end_pos = [10, -10, 15, 1/sqrt(2)]; % format [x, y, z, sin(angle)]
 point_matrix = [point_matrix; start_pos, end_pos, 10, 1, 0, 0, 0];
 
 
-start_pos = [10, -15, 20, 1/sqrt(2)];% format [x, y, z, sin(angle)]
-end_pos = [10, -15, 5, 1/sqrt(2)]; % format [x, y, z, sin(angle)]
+start_pos = [10, -10, 15, 1/sqrt(2)];% format [x, y, z, sin(angle)]
+end_pos = [10, -10, 5, 1/sqrt(2)]; % format [x, y, z, sin(angle)]
 point_matrix = [point_matrix; start_pos, end_pos, 10, 1, 0, 0, 0];
 
-start_pos = [10, -15, 5, 1/sqrt(2)];% format [x, y, z, sin(angle)]
+start_pos = [10, -10, 5, 1/sqrt(2)];% format [x, y, z, sin(angle)]
 end_pos = [10, 0, 5, 1/sqrt(2)]; % format [x, y, z, sin(angle)]
 point_matrix = [point_matrix; start_pos, end_pos, 10, 1, 0, 0, 0];
 
@@ -73,13 +73,13 @@ while i <= no_point
 [T01, T12, T23, T34, T45, T02, T03, T04, T05] = ForwardKinematics(trajectory(i,1), trajectory(i,2), trajectory(i,3), trajectory(i,4))
 
 
-
+delete(robot_body);
 [robot_body] = Simulation(trajectory(i,1), trajectory(i,2), trajectory(i,3), trajectory(i,4), x, y, z, T05(1,4),T05(2,4), T05(3,4));
 x = T05(1,4); y = T05(2,4); z = T05(3,4);
 Endposition = [Endposition; x, y, z];
 
 pause(0.2)
-delete(robot_body);
+
 i = i+1;
 end
 
